ef107351b16575fe792ce294a3067366edb5f26e
cjq <cjq02@qq.com>
2025-09-08 22:34:47 +0800

    更新.cursorrules，添加禁止出现的措辞规则

---
+++ extend/sunphp/log/mdim_log.php
@@ -0,0 +1,30 @@
+<?php
+
+if (!function_exists('mdim_log')) {
+  function mdim_log($msg)
+  {
+    $ts = date('Y-m-d H:i:s');
+    if (!is_string($msg)) {
+      $msg = json_encode($msg, JSON_UNESCAPED_UNICODE);
+    }
+    $line = '[' . $ts . '] ' . $msg;
+    @error_log($line);
+    @file_put_contents('php://stderr', $line . PHP_EOL, FILE_APPEND);
+
+    // 本地文件按 年月/日.log 落盘，自动创建目录
+    if (defined('IA_ROOT')) {
+      $logBaseDir = IA_ROOT . '/runtime/mdim/log';
+    } else {
+      $logBaseDir = __DIR__ . '/../../../../runtime/mdim/log';
+    }
+    $yearMonth = date('Ym');
+    $dayFile = date('d') . '.log';
+    $targetDir = $logBaseDir . '/' . $yearMonth;
+    if (!is_dir($targetDir)) {
+      @mkdir($targetDir, 0777, true);
+    }
+    @file_put_contents($targetDir . '/' . $dayFile, $line . PHP_EOL, FILE_APPEND);
+  }
+}
+
+
